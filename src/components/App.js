c309b50010468f69cdc7b4460c2da34b1239a94f
alvinkenyagah <alvin47k@gmail.com>
2023-04-21 12:53:45 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -4,32 +4,63 @@ import blogData from "../data/blog";
 console.log(blogData);
 
 function App() {
+  function Header(props) {
+    return (
+      <header>
+        <h1>{props.blogName}</h1>
+      </header>
+    );
+  }
 
-  const Header = React.createElement("h1", { className:"aside" }, "Overreacted"); 
-  const Name = React.createElement("p", { className:"aside" }, `Personal blog by ${blogData.name}`);
-  const About = React.createElement("p", {id:"about"}, blogData.about)
-  const Logo2 = ()=>{return(<img src={blogData.image} alt="logo" className="aside" width={56}/>)}
+  function About(props) {
+    const imgSrc = props.imageSrc || "https://via.placeholder.com/215";
+    const altText = props.altText || "blog logo";
 
-  const Post = ({title, preview, minutes}) => {
+    return (
+      <aside>
+        <img src={imgSrc} alt={altText} />
+        <p>{props.aboutText}</p>
+      </aside>
+    );
+  }
+
+  const Post = ({ title, preview, minutes }) => {
     return (
       <div className="post">
-        <h2> <a href="http://www.google.com" style={{textDecoration:"none",color:"inherit"}}>{title}</a></h2>
+        <h2>
+          {" "}
+          <a
+            href="http://www.google.com"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            {title}
+          </a>
+        </h2>
         <p>{preview}</p>
         <p>{minutes} min read</p>
       </div>
-    )
-  }
+    );
+  };
 
-  const Posted = blogData.posts.map((post)=>{
-    return <Post key={post.id} title={post.title} preview={post.preview} minutes={post.minutes} />
-  })
+  const Posted = blogData.posts.map((post) => {
+    return (
+      <Post
+        key={post.id}
+        title={post.title}
+        preview={post.preview}
+        minutes={post.minutes}
+      />
+    );
+  });
 
   return (
     <div className="App">
-      {Header}
-      <Logo2/>
-      {Name}
-      {About}
+      <Header blogName={blogData.name} />
+      <About
+        aboutText={blogData.about}
+        imageSrc={blogData.image}
+        altText="blog logo"
+      />
       {Posted}
     </div>
   );
