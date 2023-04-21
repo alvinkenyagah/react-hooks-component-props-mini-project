3472610dbb4699293387b00ff0afdae848e44247
alvinkenyagah <alvin47k@gmail.com>
2023-04-21 12:39:36 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,33 @@ import blogData from "../data/blog";
 console.log(blogData);
 
 function App() {
+
+  const Header = React.createElement("h1", { className:"aside" }, "Overreacted"); 
+  const Name = React.createElement("p", { className:"aside" }, `Personal blog by ${blogData.name}`);
+  const About = React.createElement("p", {id:"about"}, blogData.about)
+  const Logo2 = ()=>{return(<img src={blogData.image} alt="logo" className="aside" width={56}/>)}
+
+  const Post = ({title, preview, minutes}) => {
+    return (
+      <div className="post">
+        <h2>{title}</h2>
+        <p>{preview}</p>
+        <p>{minutes} min read</p>
+      </div>
+    )
+  }
+
+  const Posted = blogData.posts.map((post)=>{
+    return <Post key={post.id} title={post.title} preview={post.preview} minutes={post.minutes} />
+  })
+
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      {Header}
+      <Logo2/>
+      {Name}
+      {About}
+      {Posted}
     </div>
   );
 }
